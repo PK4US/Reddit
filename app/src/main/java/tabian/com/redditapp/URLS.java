4321d8340691f4cda8c86c6d50e074f981243505
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2021-02-19 19:51:00 +0200

    First commit

---
+++ app/src/main/java/tabian/com/redditapp/URLS.java
@@ -0,0 +1,8 @@
+package tabian.com.redditapp;
+
+public class URLS {
+
+    public static final String BASE_URL = "https://www.reddit.com/r/";
+    public static final String LOGIN_URL = "https://www.reddit.com/api/login/";
+    public static final String COMMENT_URL = "https://www.reddit.com/api/";
+}
